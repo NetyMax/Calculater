dfd91efa7dcf4aee9a0dbc69f003fed6ecf56b51
NetyMax <dl9akkov@gmail.com>
2025-06-10 20:45:19 +0300

    Create main.go
    new main

---
+++ cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com\Max\Calculation\internal"
+)
+
+func main() {
+	a, b, operation := getInput()
+
+	var result float64
+	switch operation {
+	case "+":
+		result = addition(a, b)
+
+	case "-":
+		result = subtraction(a, b)
+
+	case "*":
+		result = multiplication(a, b)
+
+	case "/":
+		result = division(a, b)
+	default:
+		fmt.Println("не известный оператор")
+		return
+	}
+	fmt.Printf("результат: %.2f/n", result)
+}
