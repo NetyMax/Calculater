4cbd65af9f786ebdee2a91a7f92387e08a50c4bc
NetyMax <dl9akkov@gmail.com>
2025-07-08 21:53:39 +0300

    goroutine

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,50 @@ import (
 	"github.com/Max/Calculation/Calculater/internal"
 )
 
+type Task struct {
+	A, B     float64
+	Operator string
+}
+
 func main() {
+
+	taskChan := make(chan Task)
+
+	go func() {
+
+		tasks := []Task{
+			{A: 5, B: 7, Operator: "+"},
+			{A: 8, B: 2, Operator: "-"},
+			{A: 2, B: 2, Operator: "*"},
+			{A: 4, B: 0, Operator: "/"},
+		}
+		for _, task := range tasks {
+			taskChan <- task
+		}
+		close(taskChan)
+	}()
+
+	go func() {
+		for task := range taskChan {
+			var result float64
+			switch task.Operator {
+			case "+":
+				result = task.A + task.B
+			case "-":
+				result = task.A - task.B
+			case "/":
+				if task.B == 0 {
+					fmt.Println("Ошибка: деление на ноль")
+					continue
+				}
+				result = task.A / task.B
+			default:
+				fmt.Println("Неизвестный оператор:", task.Operator)
+				continue
+			}
+			fmt.Printf("Операция: %.2f %s %.2f = %.2f\n", task.A, task.Operator, task.B, result)
+		}
+	}()
 
 	go func() {
 		http.HandleFunc("/Addition", internal.CalcHandler)
