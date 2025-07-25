81f9eba648431c4048a1ef2143d95eff95ba0151
NetyMax <dl9akkov@gmail.com>
2025-07-25 09:23:09 +0300

    cut func

--- internal/consumer/consumer.go
+++ internal/consumer/consumer.go
@@ -1,6 +1,7 @@
 package consumer
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/Max/Calculation/Calculater/internal/task"
@@ -8,23 +9,27 @@ import (
 
 func Start(taskChan <-chan task.Task) {
 	for t := range taskChan {
-		var result float64
-
-		switch t.Operator {
-		case "+":
-			result = t.A + t.B
-		case "-":
-			result = t.A - t.B
-		case "/":
-			if t.B == 0 {
-				fmt.Println("Ошибка: деление на ноль")
-				continue
-			}
-			result = t.A / t.B
-		default:
-			fmt.Println("Неизвестный оператор:", t.Operator)
+		result, err := DefineOperation(t)
+		if err != nil {
+			fmt.Println(err)
 			continue
 		}
 		fmt.Printf("Операция: %.2f %s %.2f = %.2f\n", t.A, t.Operator, t.B, result)
+	}
+}
+
+func DefineOperation(t task.Task) (float64, error) {
+	switch t.Operator {
+	case "+":
+		return t.A + t.B, nil
+	case "-":
+		return t.A - t.B, nil
+	case "/":
+		if t.B == 0 {
+			return 0, errors.New("ошибка: деление на ноль")
+		}
+		return t.A / t.B, nil
+	default:
+		return 0, fmt.Errorf("неизвестный оператор: %s", t.Operator)
 	}
 }
